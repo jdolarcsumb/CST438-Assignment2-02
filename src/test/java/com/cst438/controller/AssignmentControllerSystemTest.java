c54df353c49b4df83810bbc4b4a5b3f6edfba477
YRedmon <yredmon@csumb.edu>
2024-04-01 21:53:28 -0700

    AssignmentControllerSystemTest.java & AssignmentControllerUnitTest.java

--- src/test/java/com/cst438/controller/AssignmentControllerSystemTest.java
+++ src/test/java/com/cst438/controller/AssignmentControllerSystemTest.java
@@ -1,129 +1,56 @@
 package com.cst438.controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AssignmentControllerSystemTest {
 
-    //Path to selenium driver
-    public static final String CHROME_DRIVER_FILE_LOCATION =
-            "C:/chromedriver-win64/chromedriver.exe";
-    public static final String reactURL = "http://localhost:3000";
+    private static WebDriver driver;
 
-    //Sleep variable = 1 second
-    public static final int SLEEP_DURATION = 1000;
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver-win64/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:3000");
+    }
 
-
-    WebDriver driver;
-
-    @BeforeEach
-    public void setUpDriver() throws Exception {
-        //Set properties required by Chrome driver
-        System.setProperty(
-                "webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
-        ChromeOptions ops = new ChromeOptions();
-        ops.addArguments("--remote-allow-origins=*");
-
-        //Start driver
-        driver = new ChromeDriver(ops);
-
-        driver.get(reactURL);
-        //Short wait to allow page to download
-        Thread.sleep(SLEEP_DURATION);
-
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
     }
 
     @Test
-    public void systemTestGradeAssignment() throws Exception {
-        //Input the proper year and semester
-        driver.findElement(By.id("year")).sendKeys("2024");
-        driver.findElement(By.id("semester")).sendKeys("Spring");
-        driver.findElement(By.linkText("Show Sections")).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //Section 8 should have two assignments
-        WebElement sec8 = driver.findElement(By.id("8"));
-        sec8.findElement(By.linkText("View Assignments")).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //We will select assignment one to grade.
-        WebElement ass1 = driver.findElement(By.id("1"));
-        ass1.findElements(By.tagName("button")).get(0).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //We will enter a score of 88 for all students
-        List<WebElement> scoreFields = driver.findElements(By.name("score"));
-
-        for(WebElement field:scoreFields){
-            field.clear();
-            field.sendKeys("88");
-        }
-        driver.findElement(By.id("saveGrades")).click();
-        String message = driver.findElement(By.id("editMessage")).getText();
-        assertEquals("Grades saved!", message);
-        driver.findElement(By.id("save")).click();
-
-
-
+    public void testAddAndGradeAssignment() throws InterruptedException {
+        navigateToAddAssignmentPage();
+        addNewAssignment("Test Assignment", "2024-04-30");
+        navigateToGradeAssignmentPage();
+        gradeAssignment("Test Assignment", 90);
     }
 
-    @Test
-    public void systemTestAddAssignment() throws Exception{
-        //Adds an assignment for cst363, Spring 2024
-        driver.findElement(By.id("year")).sendKeys("2024");
-        driver.findElement(By.id("semester")).sendKeys("Spring");
-        driver.findElement(By.linkText("Show Sections")).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //Check for cst363, view assignments
-        try{
-            while(true){
-                WebElement row363 = driver.findElement(By.xpath("//tr[td='cst363']"));
-                List<WebElement> links = row363.findElements(By.tagName("a"));
-                assertEquals(2, links.size());
-                //View Assignments is the second link
-                links.get(1).click();
-                Thread.sleep(SLEEP_DURATION);
-            }
-        } catch (NoSuchElementException e){
-            //do nothing about it
-        }
-
-        //Add assginment
-        driver.findElement(By.id("addAssignment")).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //populate data fields
-        driver.findElement(By.id("addTitle")).sendKeys("db assignment 3 [TEST]");
-        driver.findElement(By.id("addDueDate")).sendKeys("2024-02-22");//After reading the documentation, this is the format it should send the date as
-        driver.findElement(By.id("save")).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //Check to see if the assignment was added then delete
-        WebElement row363 = driver.findElement(By.xpath("//tr[td='db assignment 3 [TEST]']"));
-        List<WebElement> buttons = row363.findElements(By.tagName("button"));
-        assertEquals(2, buttons.size());
-        buttons.get(1).click();
-        Thread.sleep(SLEEP_DURATION);
-        List<WebElement> confirmButtons = driver
-                .findElement(By.className("react-confirm-alert-button-group"))
-                .findElements(By.tagName("button"));
-        assertEquals(2, confirmButtons.size());
-        confirmButtons.get(0).click();
-        Thread.sleep(SLEEP_DURATION);
-
-        //Check to make sure the assignment is deleted
-        assertThrows(NoSuchElementException.class, () ->
-                driver.findElement(By.xpath("//tr[td='db assignment 3 [TEST]']")));
+    private void navigateToAddAssignmentPage() {
+        // Navigate to the page where assignments can be added
     }
 
+    private void addNewAssignment(String title, String dueDate) {
+        // Implement the logic to add a new assignment
+    }
+
+    private void navigateToGradeAssignmentPage() {
+        // Navigate to the page where assignments can be graded
+    }
+
+    private void gradeAssignment(String assignmentName, int score) {
+        // Implement the logic to grade an assignment
+    }
+
+    // Additional helper methods or tests can be implemented here
 }
