412fc56f9e73ec2381130db36b238b6556ff63a8
YRedmon <yredmon@csumb.edu>
2024-04-06 17:24:23 -0700

    Edited receiveFromRegistrar()

--- src/main/java/com/cst438/service/RegistrarServiceProxy.java
+++ src/main/java/com/cst438/service/RegistrarServiceProxy.java
@@ -1,5 +1,7 @@
 package com.cst438.service;
 
+import com.cst438.dto.*;
+import com.cst438.domain.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class RegistrarServiceProxy {
 
     Queue registrarServiceQueue = new Queue("registrar_service", true);
+    EnrollmentRepository enrollmentRepository;
 
     @Bean
     public Queue createQueue() {
@@ -24,6 +27,22 @@ public class RegistrarServiceProxy {
     @RabbitListener(queues = "gradebook_service")
     public void receiveFromRegistrar(String message)  {
         //TODO implement this message
+        try {
+            System.out.println("receive from Registrar " + message);
+            String[] parts = message.split(" ", 2);
+            if (parts[0].equals("updateEnrollment")) {
+                EnrollmentDTO dto = fromJsonString(parts[1], EnrollmentDTO.class);
+                Enrollment e = enrollmentRepository.findById(dto.enrollmentId()).orElse(null);
+                if (e == null) {
+                    System.out.println("Error receiveFromRegistrar Enrollment not found " + dto.enrollmentId());
+                } else {
+                    e.setGrade(dto.grade());
+                    enrollmentRepository.save(e);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in receiveFromRegistrar " + e.getMessage());
+        }
     }
 
 
