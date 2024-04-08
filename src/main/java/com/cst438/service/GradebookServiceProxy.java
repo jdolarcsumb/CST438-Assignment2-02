ad4943268f74593080e08b35382f21658be53014
Jasper Dolar <jdolar@csumb.edu>
2024-04-08 14:54:34 -0700

    CourseController updated

--- src/main/java/com/cst438/service/GradebookServiceProxy.java
+++ src/main/java/com/cst438/service/GradebookServiceProxy.java
@@ -1,12 +1,15 @@
 package com.cst438.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Service;
+import com.cst438.domain.*;
+import com.cst438.dto.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
+
 
 @Service
 public class GradebookServiceProxy {
