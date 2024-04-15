d606d029b5fe50a4ba8ecad71e9c082d650933a9
YRedmon <yredmon@csumb.edu>
2024-04-15 15:14:46 -0700

    LoginController (Done by Kevin Helm)

--- src/main/java/com/cst438/controller/LoginController.java
+++ src/main/java/com/cst438/controller/LoginController.java
@@ -3,14 +3,17 @@ package com.cst438.controller;
 import com.cst438.domain.User;
 import com.cst438.domain.UserRepository;
 import com.cst438.dto.LoginDTO;
+import com.cst438.service.TokenService;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cst438.service.TokenService;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin
@@ -29,6 +32,36 @@ public class LoginController {
 		User user = userRepository.findByEmail(name);
 		String token = tokenService.generateToken(authentication);
 		return new LoginDTO(token, user.getType());
+	}
+
+	// ADMIN function to create a new user
+	@PostMapping("/users")
+	@PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+
+	// ADMIN function to update a user
+	@PutMapping("/users/{userId}")
+	@PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
+	public User updateUser(@PathVariable("userId") int userId, @RequestBody User user) {
+		user.setUserId(userId);
+		return userRepository.save(user);
+	}
+
+	// ADMIN function to delete a user
+	@DeleteMapping("/users/{userId}")
+	@PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
+	public void deleteUser(@PathVariable("userId") int userId) {
+		userRepository.deleteById(userId);
+	}
+
+	// Get user details
+	@GetMapping("/users/{userId}")
+	@PreAuthorize("hasAnyAuthority('SCOPE_ROLE_STUDENT', 'SCOPE_ROLE_ADMIN', 'SCOPE_ROLE_INSTRUCTOR')")
+	public User getUserDetails(@PathVariable("userId") int userId) {
+		return userRepository.findById(userId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
 	}
 
 }
